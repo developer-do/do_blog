f1a180c24bbab08311a5d48a7c95b6fdc924aee0
dev-do <dev-do@naver.com>
2020-10-19 22:33:18 +0900

    router 기본 설정

--- server/app.js
+++ server/app.js
@@ -5,6 +5,23 @@ import config from "./config";
 const app = express();
 const { MONGO_URI } = config;
 
+// hpp & helmet 서버 보안 설정
+app.use(hpp());
+app.use(helmet());
+
+// cors 설정
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+// log 확인
+app.use(morgan("div"));
+
+app.use(express.json());
+
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
