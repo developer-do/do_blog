c12536d52cc2d23889f9884567c65f5377a66040
dev-do <dev-do@naver.com>
2020-10-20 21:01:55 +0900

    router(2)

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
 import mongoose from "mongoose";
+import morgan from "morgan";
 import config from "./config";
+import cors from "cors";
+
+// Routes
+import postsRoutes from "./routes/api/posts";
 
 const app = express();
 const { MONGO_URI } = config;
@@ -26,9 +33,13 @@ mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log("MongoDB connecting Success!!"))
   .catch((e) => console.log(e));
+
+// Use routes
 app.get("/");
+app.use("/api/post", postsRoutes);
 
 export default app;
